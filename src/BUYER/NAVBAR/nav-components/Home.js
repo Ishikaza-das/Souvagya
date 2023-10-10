e445a5a41aa3c027f9c2db7a43aeda4359dea6cf
SouvagyaDash29 <souvagyadash1999@gmail.com>
2023-10-10 20:10:36 +0530

    Adding Slider

--- src/BUYER/NAVBAR/nav-components/Home.js
+++ src/BUYER/NAVBAR/nav-components/Home.js
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import "./style/Home.css";
 const Home = () => {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
   return (
-    <div>Home</div>
+    <div className='home-section'>
+       <Slider {...settings} className='slider'>
+        <div className='random'>
+          <h3>Slide 1</h3>
+        </div>
+        <div className='random'>
+          <h3>Slide 2</h3>
+        </div>
+        <div className='random'>
+          <h3>Slide 3</h3>
+        </div>
+      </Slider>
+    </div>
   )
-}
+ }
+
 
 export default Home

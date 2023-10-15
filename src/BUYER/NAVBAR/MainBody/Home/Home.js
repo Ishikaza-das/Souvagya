1fbfce179df27dfc5865536ac7d8bccb7ad05368
SouvagyaDash29 <souvagyadash1999@gmail.com>
2023-10-15 14:42:56 +0530

    Add 2 section (Best selling and hot price)

--- src/BUYER/NAVBAR/MainBody/Home/Home.js
+++ src/BUYER/NAVBAR/MainBody/Home/Home.js
@@ -3,14 +3,9 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Home.css";
-import Record from "../categories.json"
-import { Link, Routes, Route } from "react-router-dom";
-import Man from "../Categories/Subcomponents/Man";
-import Woman from "../Categories/Subcomponents/Woman";
-import Kids from "../Categories/Subcomponents/Kids";
-import Electronics from "../Categories/Subcomponents/Electronics";
-import HomeApplience from "../Categories/Subcomponents/HomeApplience";
-import BestOffer from "../Categories/Subcomponents/BestOffer";
+import Record from "../categories.json";
+import Selling from "../BestSelling.json"
+import { Link } from "react-router-dom";
 
 
 const settings = {
@@ -22,45 +17,82 @@ const settings = {
 };
 const Home = () => {
   return (
-      <div className="home-section">
-        <Slider {...settings} className="slider">
-          <div className="random">
-            <h3>Slide 1</h3>
-          </div>
-          <div className="random">
-            <h3>Slide 2</h3>
-          </div>
-          <div className="random">
-            <h3>Slide 3</h3>
-          </div>
-        </Slider>
-        <div className="categorie-section">
-          {
-            Record.map( record => {
-              return(
-                <div key={record.id}>
-                  <Link to={record.Link} style={{ textDecoration: "none" }}>
-                  <div className="section">
+    <div className="slider-section">
+      <Slider {...settings} className="slider">
+        <div className="random">
+          <h3>Slide 1</h3>
+        </div>
+        <div className="random">
+          <h3>Slide 2</h3>
+        </div>
+        <div className="random">
+          <h3>Slide 3</h3>
+        </div>
+      </Slider>
+      <div className="categorie-section">
+        {Record.map((record) => {
+          return (
+            <div key={record.id}>
+              <Link to={record.Link} style={{ textDecoration: "none" }}>
+                <div className="section">
                   <img src={record.image} alt="img" />
                   <h4>{record.name}</h4>
-                  </div>
-                  </Link>
                 </div>
-              )
-            })
-          }
-        </div>
-        <Routes>
-        <Route path="/Man" element={<Man />} />
-        <Route path="/Woman" element={<Woman />} />
-          <Route path="/Kid" element={<Kids />} />
-          <Route path="/Electronic" element={<Electronics />} />
-          <Route path="/Home_applience" element={<HomeApplience />} />
-          <Route path="/Best_offer" element={<BestOffer />} />
-
-        </Routes>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-  )
-}
+     <div className="categorie-section1">
+      <h3>Best Selling</h3>
+      <div className="best-selling">
+        {Selling.map((selling) => {
+          return(
+            <div key={selling.id} >
+              <Link to="*" style={{ textDecoration: "none" }}>
+                <div className="section1">
+                  <img src={selling.image} alt="img" />
+                  <h4>{selling.name}</h4>
+                  <div className="price">
+                    <h6>{selling.price}</h6>
+                    <h6>{selling.discount}</h6>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          )
+        })
 
-export default Home
+        }
+      </div>
+
+     </div>
+     <div className="categorie-section2">
+      <h3>Hot Price</h3>
+      <div className="best-selling">
+        {Selling.map((selling) => {
+          return(
+            <div key={selling.id} >
+              <Link to="*" style={{ textDecoration: "none" }}>
+                <div className="section1">
+                  <img src={selling.image} alt="img" />
+                  <h4>{selling.name}</h4>
+                  <div className="price">
+                    <h6>{selling.price}</h6>
+                    <h6>{selling.discount}</h6>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          )
+        })
+
+        }
+      </div>
+
+     </div>
+    </div>
+  );
+};
+
+export default Home;

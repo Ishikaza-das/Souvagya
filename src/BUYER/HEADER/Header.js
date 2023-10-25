0dd38ee7b82c8593f657ae67235539e50b616a67
Ritesh <riteshdas8652@gmail.com>
2023-10-25 19:30:39 +0530

    User Nav

--- src/BUYER/HEADER/Header.js
+++ src/BUYER/HEADER/Header.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import { RiShoppingCartLine } from 'react-icons/ri';
-import { BiUserCircle } from 'react-icons/bi';
-import { FaClipboardList } from 'react-icons/fa';
+import React from "react";
+import { RiShoppingCartLine } from "react-icons/ri";
+// import { BiUserCircle } from "react-icons/bi";
+import { FaClipboardList } from "react-icons/fa";
 // import { IconContext } from "react-icons";
 import "./Header.css";
-
+import UserNav from "./User/UserNav";
 
 const Header = () => {
   return (
-
-    <div className ='header-section'>
-      <div className='logo-section'>
-        Quick Mart and Logo
+    <div className="header-section">
+      <div className="logo-section">Quick Mart and Logo</div>
+      <div className="search-bar">
+        <input
+          placeholder="Search.."
+          className="input"
+          name="text"
+          type="text"
+        />
       </div>
-       <div className='search-bar'> 
-      <input placeholder="Search.."  className="input" name="text" type="text" />
-      </div>
-    <div className='icon-section'>
+      <div className="icon-section">
         <RiShoppingCartLine />
         <FaClipboardList />
-        <BiUserCircle />
+        {/* <BiUserCircle /> */}
+        <UserNav />
+      </div>
     </div>
-    </div>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;

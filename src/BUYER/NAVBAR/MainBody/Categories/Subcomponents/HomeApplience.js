79e82d63e21bc7db18402948e3600c8d01847f5f
subhankar <subhankarbehera48@gmail.com>
2023-11-20 11:42:06 +0530

    add home appliance

--- src/BUYER/NAVBAR/MainBody/Categories/Subcomponents/HomeApplience.js
+++ src/BUYER/NAVBAR/MainBody/Categories/Subcomponents/HomeApplience.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Mans from "./Mans.json";
+import HomesAppliances from "./HomeAppliance.json";
 import "./Comp-style/Man.css";
 import { AiFillHeart } from "react-icons/ai";
 import { ImCancelCircle } from "react-icons/im";
@@ -45,41 +45,41 @@ const HomeApplience = () => {
   return (
     <>
       <div className='Man-section' >
-        {Mans.map((mans,index) => {
+        {HomesAppliances.map((homea,index) => {
           return (
             <>
-              <div className='Man' key={mans.id}>
-                <div className='man-section' onClick={() => toggleSecondDiv(mans.id)} >
-                  <img src={mans.image} alt="img"  />
+              <div className='Man' key={homea.id}>
+                <div className='man-section' onClick={() => toggleSecondDiv(homea.id)} >
+                  <img src={homea.image} alt="img"  />
                   <div className='tittle'>
-                    <h5>{mans.name} </h5>
-                    <h6>{mans.price}<span className="like"> <AiFillHeart /> </span></h6>
+                    <h5>{homea.name} </h5>
+                    <h6>{homea.price}<span className="like"> <AiFillHeart /> </span></h6>
                     
                   </div>
                 </div>
               </div>
-              {selectedItemId === mans.id && (
+              {selectedItemId === homea.id && (
                 <div className="test" >
                   {/* Image carousel */}
                   <div className="image-carousel">
                     <Slider {...settings} className="slider" key={index}>
                       <div className="sliders">
-                        <img src={mans["thubnail-1"]} alt="img" />
+                        <img src={homea["thubnail-1"]} alt="img" />
                       </div>
                       <div className="sliders">
-                        <img src={mans["thubnail-2"]} alt="img" />
+                        <img src={homea["thubnail-2"]} alt="img" />
                       </div>
                       <div className="sliders">
-                        <img src={mans["thubnail-3"]} alt="img" />
+                        <img src={homea["thubnail-3"]} alt="img" />
                       </div>
                     </Slider>
 
-                    <h4 className="product-name">{mans.name}</h4>
+                    <h4 className="product-name">{homea.name}</h4>
 
                     {/* Buttons for Add to Cart and Buy Now */}
                     <div className="buttons">
                     <button onClick={buyNow} className="Button">Buy Now</button>
-                      <button onClick={() => addToCart(mans)} className="button type1">
+                      <button onClick={() => addToCart(homea)} className="button type1">
                       <div className="btn-txt">Add to cart</div>
                       </button>
                       <h3  onClick={close}> <ImCancelCircle /></h3>

e7b3e6aef4f90eda9076c006f8e8d2b7d2b4a681
Ritesh <riteshdas8652@gmail.com>
2024-01-29 17:02:43 +0530

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
-import Header from "./BUYER/HEADER/Header"
-import Navbar from "./BUYER/NAVBAR/Navbar"
-import Footer from "./BUYER/FOOTER/Footer"
+import Header from "./BUYER/HEADER/Header";
+import Navbar from "./BUYER/NAVBAR/Navbar";
+import Footer from "./BUYER/FOOTER/Footer";
+import { useState } from "react";
 // import App1 from "./VENDOR/App1";
 
 function App() {
@@ -13,13 +14,13 @@ function App() {
 
   return (
     <div>
-          <Header />
-        
-        <Navbar />
-  
-        <Footer /> 
-         
-           {/* <App1/>    */}
+      <Header />
+
+      <Navbar />
+
+      <Footer />
+
+      {/* <App1/>    */}
     </div>
   );
 }
